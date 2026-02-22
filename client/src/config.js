56db6e3541bd67caf7e7d0fccecdc625b493ff62
raheemabdul24 <raheemabdul29650@gmail.com>
2026-02-22 17:27:02 +0530

    allow all origins when CLIENT_URL not set

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api'
+export const API_BASE = import.meta.env.VITE_API_BASE || 'https://alumni-connect-student-and-alumni.onrender.com/api'
